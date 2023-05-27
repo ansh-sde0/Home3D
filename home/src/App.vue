b51114d3377b7907cb4572c01009359f3e9ec27d
Ansh Tyagi <ansh.tyagi@mylofamily.com>
2023-05-27 19:59:39 +0530

    Start

---
+++ home/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HomeInfra/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HomeInfra from './components/Home.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HomeInfra
+  }
+});
+</script>
+<style>
+* {
+  padding:0;
+  margin:0;
+  background-color: black;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 6px;
+}
+</style>
---
+++ home/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Home } from '@/babylon/home'
+
+export default defineComponent({
+  name: 'HomeInfra',
+  mounted() {
+    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+    new Home(canvas)
+  }
+});
+</script>
+<style scoped>
+main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+canvas {
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  box-shadow: 8px 8px 10px -6px #000000;
+}
+</style>
